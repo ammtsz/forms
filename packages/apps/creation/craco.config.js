f40afd9bde5aa127df9ac6eed5c29c79fbfec9e0
ammtsz <am.matsuzoe@hotmail.com>
2023-06-29 16:26:27 -0700

    fix alias path on module federation modules

--- packages/apps/creation/craco.config.js
+++ packages/apps/creation/craco.config.js
@@ -1,8 +1,39 @@
 const path = require('path');
+const { dependencies } = require("./package.json");
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
+
 module.exports = {
   webpack: {
     alias: {
       '@app': path.resolve(__dirname, 'src'),
     },
+    configure: {
+      output: {
+        publicPath: 'auto'
+      }
+    },
+    plugins: {
+      add: [new ModuleFederationPlugin(
+        (module.exports = {
+          name: "creation",
+          exposes: {
+            "./CreationPage": "./src/pages/FormCreation",
+          },
+          filename: "remoteEntry.js",
+          shared: {
+            ...dependencies,
+            react: {
+              singleton: true,
+              requiredVersion: dependencies["react"],
+            },
+            "react-dom": {
+              singleton: true,
+              requiredVersion: dependencies["react-dom"],
+            },
+          },
+        })
+      )]
+    }
   },
 };
