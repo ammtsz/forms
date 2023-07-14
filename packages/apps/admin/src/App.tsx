22576cc8c3fb805f37b80264fbf0ce5ae39f9486
ammtsz <am.matsuzoe@hotmail.com>
2023-07-14 16:49:42 -0700

    refactor(admin): rename to Home page

--- packages/apps/admin/src/App.tsx
+++ packages/apps/admin/src/App.tsx
@@ -1,13 +1,9 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-// import { createBrowserHistory } from 'history'
-import FormManagementPage from "./pages/Home";
-
+const HomePage = lazy(() => import("./pages/Home"));
 const FormCreationPage = lazy(() => import("./pages/Creation"));
 const FormViewPage = lazy(() => import("./pages/View"));
-
-// const history = createBrowserHistory()
 
 const App = () => {
   return (
@@ -17,7 +13,7 @@ const App = () => {
           <Routes>
             <Route path="/create" element={<FormCreationPage />} />
             <Route path="/view" element={<FormViewPage />} />
-            <Route path="/" element={<FormManagementPage />} />
+            <Route path="/" element={<HomePage />} />
           </Routes>
         </Suspense>
       </div>
