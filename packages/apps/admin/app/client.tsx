58d73686342156874f0e4fd2ae32ee1a940d9239
ammtsz <am.matsuzoe@hotmail.com>
2023-09-03 11:33:45 -0700

    fix(admin): create client component for layout file

---
+++ packages/apps/admin/app/client.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Suspense } from "react";
+
+import Alert from "@app/components/Alert";
+import Breadcrumb from "@app/components/Breadcrumb";
+import Feedback from "@app/components/Feedback";
+import Navbar from "@app/components/Navbar";
+import Providers from "@app/components/Providers";
+
+import "./globals.css";
+
+const ClientComponent: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <Suspense fallback={<Feedback isLoading />}>
+      <Providers>
+        <Navbar />
+        <Alert />
+        <Breadcrumb />
+        {children}
+      </Providers>
+    </Suspense>
+  );
+};
+
+export default ClientComponent;
--- packages/apps/admin/app/layout.tsx
+++ packages/apps/admin/app/layout.tsx
@@ -1,16 +1,9 @@
-"use client";
-
 import type { Metadata } from "next";
-import { Suspense } from "react";
 
-import Alert from "@app/components/Alert";
-import Breadcrumb from "@app/components/Breadcrumb";
-import Feedback from "@app/components/Feedback";
-import Navbar from "@app/components/Navbar";
-import Providers from "@app/components/Providers";
 import { languages } from "@app/i18n/settings";
 
 import "./globals.css";
+import ClientComponent from "./client";
 
 export const metadata: Metadata = {
   title: "Forms | Admin",
@@ -27,14 +20,7 @@ const RootLayout: React.FC<{
   return (
     <html lang={lng}>
       <body>
-        <Suspense fallback={<Feedback isLoading />}>
-          <Providers>
-            <Navbar />
-            <Alert />
-            <Breadcrumb />
-            {children}
-          </Providers>
-        </Suspense>
+        <ClientComponent>{children}</ClientComponent>
       </body>
     </html>
   );
